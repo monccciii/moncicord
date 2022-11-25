d2cc29ccdf849c872bc028e38bb100571e9d2c7d
monccciii <44330082+monccciii@users.noreply.github.com>
2022-11-24 23:35:21 -0500

    Add files via upload

---
+++ frontend/src/components/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useNavigate} from 'react-router-dom';
+import {useEffect, useState} from "react";
+import axios from 'axios';
+import { Button, Form } from "react-bootstrap";
+
+
+function Home() {
+    return (
+      <div style={{width:'90%', margin:'auto auto', textAlign:'center'}}>
+            <h1>Home</h1>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
