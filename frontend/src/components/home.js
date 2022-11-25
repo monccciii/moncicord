d0332fe2baa72c1bb5ae3c5b58626a895ee2fcfd
monccciii <44330082+monccciii@users.noreply.github.com>
2022-11-25 01:30:27 -0500

    new home page :)
    incomplete but functional and responsive :)

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -2,24 +2,44 @@ import React from 'react';
 import { useNavigate} from 'react-router-dom';
 import {useEffect, useState} from "react";
 import axios from 'axios';
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Container, Row, Col, Image} from "react-bootstrap";
 
+import ico from '../styles/images/discordicon.png';
+import backgroundimg1 from '../styles/images/backgroundimg1.png';
 
 function Home() {
     return (
-      <div style={{width:'90%', margin:'auto auto', textAlign:'center'}}>
-            <h1>Home</h1>
-            <Button
-            style={{marginRight:'3vw'}}
-            href='/login'
-            variant='outline-dark'>
-            Login
-            </Button>
-            <Button
-            href='/register'
-            variant='outline-dark'>
-            Register
-            </Button>
+      <div>
+        <div className='upperdiv'>
+            <Container>
+                <Row>
+                    <Col>
+        <nav>
+            <ul className='navbar_'>
+            <img className='ico' src={ico}></img>
+                <li className='companyname'>Discord</li>
+                <Button className='buttonb' variant='outline-light' href='/login'>Login</Button>
+                <Button className='buttonb' variant='outline-light' href='/register'>Register</Button>
+            </ul>
+        </nav>
+            <Image className='responsiveimg1' src={backgroundimg1}></Image>
+            <p className='upperdivtext'>Available on all devices.</p>
+            <p className='upperdivtext'>Anytime.</p>
+            <p className='upperdivtext'>&nbsp;Anywhere.</p>
+                    </Col>
+                </Row>
+        </Container>
+        </div>
+        <div className='lowerdiv'>
+            <Container>
+                <Row>
+                    <Col>
+                    <h1 style={{textAlign:'center', marginTop:'5vh'}}>Imagine a place where you can be you.</h1>
+
+                    </Col>
+                </Row>
+            </Container>
+        </div>
       </div>
     );
   }
