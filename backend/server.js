ae40410d0312a61aead54d96527b102b8ada0fbb
monccciii <44330082+monccciii@users.noreply.github.com>
2022-11-26 02:55:43 -0500

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
 const app = express();
 
 app.use(express.json());
@@ -12,49 +11,93 @@ mongoose
 .connect("mongodb+srv://spike:npoog0101@cluster0.52okq1r.mongodb.net/?retryWrites=true&w=majority")
 .catch((err) => console.log(err));
 
-const postSchema = mongoose.Schema({
-    title: String,
-    description: String
+
+//accountcreation and login
+
+const accountSchema = mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
 })
 
-const Post = mongoose.model("Post", postSchema)
+const Account = mongoose.model("Account", accountSchema)
 
 app.get("/", (req, res) => {
-    res.send("Express is here");
+    res.send("test")
 });
 
-app.post("/create", (req, res) => {
-    Post.create({
-        title: req.body.title,
-        description: req.body.description
+app.post("/register", (req, res) => {
+    Account.create({
+        username: req.body.username,
+        password: req.body.password
+    }). then(doc => console.log(doc))
+    .catch(err => console.log(err));
+})
+
+app.post("/login", (req, res) => {
+    const findname = req.body.username
+    const checkpass = req.body.password
+    Account.find({username: findname, password: checkpass}, (error, data) => {
+        if(error) {
+            console.log(error)
+        } else {
+            console.log(data)
+            if (data.length < 1) {
+                console.log("Incorrect Password or Username")
+                res.json(false)
+            } else {
+                console.log("Logged in")
+                res.json(true)
+            }
+        }
+    })
+})
+
+//chatschmea creation/read/update/delete
+
+const messageSchema = mongoose.Schema({
+    from: { type: String, required: true},
+    messagecontent: { type: String, required: true }
+})
+
+const Message = mongoose.model("Message", messageSchema)
+
+app.post("/sendmessage", (req, res) => {
+    Message.create({
+        from: req.body.from,
+        messagecontent: req.body.chat
     }). then(doc => console.log(doc))
     .catch(err => console.log(err));
 });
 
-app.delete("/delete/:id", (req, res) => {
-    Post.findByIdAndDelete({_id: req.params.id})
-    .then((doc) => console.log(doc))
-    .catch((err) => console.log(err));
-    });
 
-app.put("/update/:id", (req, res) => {
-    Post.findByIdAndUpdate(
-        {_id: req.params.id}, 
-        {
-        title: req.body.title,
-        description: req.body.description,
-        }
-    )
-    .then((doc) => console.log(doc))
-    .catch((err) => console.log(err));
-    })
-
-app.get("/posts", (req, res) => {
-    Post.find()
+app.get("/messages", (req, res) => {
+    Message.find()
     .then((items) => res.json(items))
     .catch((err) => console.log(err))
 });
 
-app.listen(3001, function () {
-    console.log("server is running");
+const chatroomSchema = mongoose.Schema({
+    name: { type: String, required: true},
+})
+
+const Chatroom = mongoose.model("Chatroom", chatroomSchema)
+
+app.post("/createchatroom", (req, res) => {
+    Chatroom.create({
+        name: req.body.chatroomname
+    }). then(doc => console.log(doc))
+    .catch(err => console.log(err));
+});
+
+
+app.get("/chatrooms", (req, res) => {
+    Chatroom.find()
+    .then((items) => res.json(items))
+    .catch((err) => console.log(err))
+});
+
+
+
+app.listen(3001, function() {
+    console.log("server is running")
 })
